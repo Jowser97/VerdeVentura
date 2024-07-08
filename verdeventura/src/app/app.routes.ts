7d7638abcb11e3f330bbd53204a954e43bcc55c6
Jowser97 <160051907+Jowser97@users.noreply.github.com>
2024-07-08 13:09:20 +0200

    cositas hechas

--- verdeventura/src/app/app.routes.ts
+++ verdeventura/src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { PathLocationStrategy } from '@angular/common';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { Err404Component } from './pages/err-404/err-404.component';
+import { RetosComponent } from './pages/retos/retos.component';
+import { UserprofileComponent } from './pages/userprofile/userprofile.component';
 
 export const routes: Routes = [
-{path: '', component: LoginComponent},
+{path: '', component: Err404Component},
+{path: 'retos', component: RetosComponent},
+{path: 'login', component: LoginComponent},
+{path: 'perfil', component: UserprofileComponent},
+{path: '**', component: Err404Component},
+
 ];
