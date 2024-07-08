2dc1eaad1f8fa91d0975185b03e866599bf33fc9
Jowser97 <160051907+Jowser97@users.noreply.github.com>
2024-07-08 11:27:09 +0200

    profile
    hay que hacer los routerlinks

--- verdeventura/src/app/app.routes.ts
+++ verdeventura/src/app/app.routes.ts
@@ -1,3 +1,7 @@
+import { PathLocationStrategy } from '@angular/common';
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '', component: LoginComponent},
+];
