dd0f39d058a7aeade7b04c93e89ab622cbc8b9f9
Jowser97 <160051907+Jowser97@users.noreply.github.com>
2024-07-09 09:38:26 +0200

    paths y cosas

--- verdeventura/src/app/app.routes.ts
+++ verdeventura/src/app/app.routes.ts
@@ -4,11 +4,16 @@ import { LoginComponent } from './pages/login/login.component';
 import { Err404Component } from './pages/err-404/err-404.component';
 import { RetosComponent } from './pages/retos/retos.component';
 import { UserprofileComponent } from './pages/userprofile/userprofile.component';
+import { RankingComponent } from './pages/ranking/ranking.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GuiaodsComponent } from './pages/guiaods/guiaods.component';
 
 export const routes: Routes = [
-  { path: '', component: Err404Component },
+  { path: '', component: HomeComponent },
   { path: 'retos', component: RetosComponent },
   { path: 'login', component: LoginComponent },
   { path: 'perfil', component: UserprofileComponent },
+  { path: 'rank', component: RankingComponent },
+  { path: 'guiaods', component: GuiaodsComponent },
   { path: '**', component: Err404Component },
 ];
---
+++ verdeventura/src/app/pages/guiaods/guiaods.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guiaods',
+  standalone: true,
+  imports: [],
+  templateUrl: './guiaods.component.html',
+  styleUrl: './guiaods.component.css'
+})
+export class GuiaodsComponent {
+
+}
